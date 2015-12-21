08198708c2572d182c946ea3f8c1a4a12e4abc1c
Alexander <alexandrevtukh@gmail.com>
2015-12-21 22:37:49 +0200

    Part 1

---
+++ Weather/DailyWeather.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Weather
+{
+    public struct DailyWeather
+    {
+        #region Constructors
+
+        public DailyWeather( long day , int min , int max )
+            :this()
+        {
+            Day = day;
+            Max = max;
+            Min = min;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public long Day
+        {
+            get;
+            private set;
+        }
+
+        public int Max
+        {
+            get;
+            private set;
+        }
+
+        public int Min
+        {
+            get;
+            private set;
+        }
+
+        public int Difference
+        {
+            get
+            {
+                return Max - Min;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return String.Format( "Day: {0}, Min: {1}, Max: {2}, Difference: {3}" , Day , Min , Max , Difference );
+        }
+
+        #endregion
+    }
+}
---
+++ Weather/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace Weather
+{
+    class Program
+    {
+        #region Fieds
+
+        private static Parser parser;
+
+        private static char splitter;
+
+        #endregion
+
+        #region Methods
+
+        static void Main( string[] args )
+        {
+            DailyWeather minDifferenceWeather;
+
+            splitter = '\t';            
+            parser = new Parser();
+
+            minDifferenceWeather = GetMinDifferenceWeather( args[ 0 ] );
+
+            Console.WriteLine(String.Format("{0} day is the day with min difference between Max and Min temperature equal {1}.", 
+                minDifferenceWeather.Day, minDifferenceWeather.Difference));
+        }
+
+        private static DailyWeather GetMinDifferenceWeather( string filePath )
+        {
+            StreamReader reader;
+            DailyWeather resultWeather;
+            DailyWeather currentWhether;
+
+            using (reader = new StreamReader(filePath))
+            {
+                reader.ReadLine();
+                resultWeather = parser.Parse(reader.ReadLine(), splitter);
+
+                while (!reader.EndOfStream)
+                {
+                    currentWhether = parser.Parse(reader.ReadLine(), splitter);
+
+                    if (currentWhether.Difference < resultWeather.Difference)
+                    {
+                        resultWeather = currentWhether;
+                    }
+                }                
+            }
+            return resultWeather;
+        }
+
+        #endregion
+    }
+}
